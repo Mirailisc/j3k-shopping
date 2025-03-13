26fe376e8a26b18aedf81c2d60dd61cdc4cdec79
Mirailisc <mirailisclm@gmail.com>
2025-03-13 21:52:58 +0700

    asdfasdfasdf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
 
   app.use(cookieParser())
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false,
   })
