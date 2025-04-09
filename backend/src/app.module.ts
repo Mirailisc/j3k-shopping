dcd40b543c2c4875f9df4f8f5ff003468f711004
Mirailisc <mirailisclm@gmail.com>
2025-04-10 00:02:15 +0700

    backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,23 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { ConfigModule } from '@nestjs/config'
-import { ProfileModule } from './profile/profile.module';
-import { SocialModule } from './social/social.module';
-import { ContactModule } from './contact/contact.module';
-import { ProductModule } from './product/product.module';
+import { ProfileModule } from './profile/profile.module'
+import { SocialModule } from './social/social.module'
+import { ContactModule } from './contact/contact.module'
+import { ProductModule } from './product/product.module'
+import { OrderModule } from './order/order.module'
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigModule.forRoot(), ProfileModule, SocialModule, ContactModule, ProductModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot(),
+    ProfileModule,
+    SocialModule,
+    ContactModule,
+    ProductModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/order/enum/order.enum.ts
@@ -0,0 +1,10 @@
+export enum OrderStatus {
+  PENDING = 'Pending',
+  PAID = 'Paid',
+  SHIPPED = 'Shipped',
+  DELIVERING = 'Delivering',
+  COMPLETED = 'Completed',
+  CANCELLED = 'Cancelled',
+  REFUNDED = 'Refunded',
+  REFUNDING = 'Refunding',
+}
