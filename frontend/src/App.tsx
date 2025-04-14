1ecb80c59903c7295596b80f2d5b90afc9b70950
Mirailisc <mirailisclm@gmail.com>
2025-04-15 00:04:57 +0700

    ghjghj

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -56,8 +56,8 @@ function App() {
 
   const navbarRender = () => {
     if (location.pathname.includes(ADMIN_BASE_PATH)) return <AdminNavbar />
-    else if (location.pathname === SIGN_IN_PATH) return <></>
-    else if (location.pathname === SIGN_UP_PATH) return <></>
+    else if (location.pathname === SIGN_IN_PATH && !isAuthenticated) return <></>
+    else if (location.pathname === SIGN_UP_PATH && !isAuthenticated) return <></>
     else return <Navbar />
   }
 
