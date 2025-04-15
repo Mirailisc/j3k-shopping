f5798f802df7315a509cac936aec72ff00b43aeb
Jirat Arayapitak <ham.jirat24@gmail.com>
2025-04-15 13:16:58 +0700

    review management backend(krob laew mung) revision frontend

--- backend/src/review/review.controller.ts
+++ backend/src/review/review.controller.ts
@@ -4,24 +4,97 @@ import {
   Delete,
   Get,
   Param,
+  Patch,
   Post,
   Put,
   Request,
-  UploadedFile,
   UseGuards,
 } from '@nestjs/common'
-import { ReviewService } from './review.service'
-import { AuthGuard } from '@nestjs/passport'
 import { AdminGuard } from 'src/auth/admin.guard'
-import { UpdateReviewDto } from './dto/update-review.dto'
+import { SuperAdminGuard } from 'src/auth/super-admin.guard'
+import { AuthGuard } from '@nestjs/passport'
 import { CreateReviewDto } from './dto/create-review.dto'
+import { ReviewService } from './review.service';
+import { UpdateReviewDto } from './dto/update-review.dto'
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Get()
-  async getAllReview() {
+  @Get('admin')
+  @UseGuards(AuthGuard('jwt'), AdminGuard)
+  async getAllReview(){
     return await this.reviewService.getAllReview()
   }
+
+  @Get(":id")
+  async getReviewById(@Param('id') id: string){
+    return await this.reviewService.getReviewById(id)
+  }
+
+  @Get("search/:id")
+  async getReviewByProductId(@Param('id') id: string){
+    return await this.reviewService.getReviewByProductId(id)
+  }
+
+  @Get("buyer/:id")
+  async getReviewByUserId(@Param('id') id : string){
+    return await this.reviewService.getReviewByUserId(id)
+  }
+
+  @Post("/buyer")
+  @UseGuards(AuthGuard('jwt'))
+  async createReviewByBuyer(
+    @Request() req,
+    @Body() review: CreateReviewDto
+  ){
+    return await this.reviewService.createReviewByBuyer(review, req.user.userId)
+  }
+
+  @Post("/admin")
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
+  async createReviewByAdmin(
+    @Body() review: CreateReviewDto
+  ){
+    return await this.reviewService.createReviewByAdmin(review)
+  }
+
+  @Delete('buyer/:id')
+  @UseGuards(AuthGuard('jwt'))
+  async deleteReviewByBuyer(
+    @Param('id') id:string,
+    @Request() req
+  ){
+      return await this.reviewService.deleteReviewByBuyer(id,req.user.userId)
+  }
+
+  @Delete("admin/:id")
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
+  async deleteReviewByAdmin(
+    @Param('id') id:string
+  ){
+    return await this.reviewService.deleteReviewByAdmin(id)
+  }
+
+  @Patch("buyer/:id")
+  @UseGuards(AuthGuard('jwt'))
+  async updateReviewByBuyer(
+    @Param('id') id:string,
+    @Request() req,
+    @Body() review: UpdateReviewDto
+  ){
+    return await this.reviewService.updateReviewByBuyer(id, review, req.user.userId)
+  }
+
+  @Patch("admin/:id")
+  @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
+  async updateReviewByAdmin(
+    @Param('id') id:string,
+    @Body() review: UpdateReviewDto
+  ){
+    return await this.reviewService.updateReviewByAdmin(id, review)
+  }
+
 }
+
+
--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,23 +1,133 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, Logger } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { Review } from './entities/review.entity'
-import { CreateReviewDto } from './dto/create-review.dto'
-import { UpdateReviewDto } from './dto/update-review.dto'
-import { User } from 'src/user/entities/user.entity'
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
 import { randomUUID } from 'crypto'
+import { Order } from '@prisma/client';
 
 @Injectable()
 export class ReviewService {
   constructor(private readonly prisma: PrismaService) {}
-
+  
   async getAllReview() {
-    const reviews = await this.prisma.$queryRaw<Review[]>`
+    const result = await this.prisma.$queryRaw<Review[]>`
       SELECT *
       FROM Review
     `
-
-    return reviews.map((review) => ({
-      ...review,
-    }))
+    return result
   }
+
+  async getReviewById(id: string){
+    const review = await this.prisma.$queryRaw<Review[]>
+      `SELECT * FROM REVIEW WHERE id = ${id}`
+    
+    if(!review || review.length === 0){
+      throw new Error('Review not found')
+    }
+
+    return review[0]
+  }
+
+  async getReviewByProductId(id: string){
+    const review = await this.prisma.$queryRaw<Review[]>
+      `SELECT * FROM REVIEW WHERE prudctId = ${id}`
+
+    return review
+  }
+
+  async getReviewByUserId(id: string){
+    const review = await this.prisma.$queryRaw<Review[]>
+      `SELECT * FROM REVIEW WHERE userId = ${id}`
+
+    return review
+  }
+
+  async createReviewByBuyer(review: CreateReviewDto, me:string){
+    const uuid =randomUUID()
+    const existingOrder = await this.prisma.$queryRaw<Order[]>
+  `  SELECT id 
+    FROM \`Order\` o
+    WHERE o.productId = ${review.productId} 
+    AND o.userId = ${review.userId}
+    `
+
+    if(!existingOrder || existingOrder.length === 0){
+      throw new Error("You have not order this product yet!")
+    }
+
+    if(existingOrder[0].userId !== me ){
+      throw new Error("You can not review did product.")
+    }
+    return await this.prisma.$executeRaw<Review[]> `
+      INSERT INTO Review (id, rating, comment, userId, productId) VALUES
+      (${uuid}, ${review.rating}, ${review.comment}, ${review.userId}, ${review.productId})
+    `
+
+  }
+
+  async createReviewByAdmin(review: CreateReviewDto){
+    const uuid = randomUUID()
+    const existingOrder = await this.prisma.$queryRaw<Order[]>
+      `SELECT id
+      FROM \`Order\` o
+      WHERE o.userId = ${review.userId} AND o.productId = ${review.productId}
+    `
+    if (!existingOrder || existingOrder.length === 0) {
+      throw new BadRequestException("User haven't order this before")
+    }
+
+    await this.prisma.$executeRaw<Review[]>
+      `INSERT INTO Review (id, rating, comment, userId, productId) VALUES
+      (${uuid}, ${review.rating}, ${review.comment}, ${review.userId}, ${review.productId})
+    `
+  }
+
+  async deleteReviewByBuyer(id:string, me:string) {
+    const reviewInfo = await this.getReviewById(id)
+    if(reviewInfo.userId !== me){
+      throw new BadRequestException("You are not the owner of this review")
+    }
+    return await this.prisma.$executeRaw
+      `DELETE FROM Review WHERE id = ${id}`
+    
+  }
+
+  async deleteReviewByAdmin(id:string) {
+    return await this.prisma.$executeRaw
+      `DELETE FROM Review WHERE id = ${id}`
+    
+  }
+
+  async updateReviewByBuyer(id:string, review: UpdateReviewDto, me: string ){
+    const reviewInfo = await this.getReviewById(id)
+    if(reviewInfo.userId !== me) {
+      throw new BadRequestException("You are not the owner of this review")
+    }
+
+    await this.prisma.$executeRaw<Review[]>
+      `UPDATE Review
+      SET rating = ${review.rating}, comment = ${review.comment}
+      WHERE id = ${id}`
+    
+    return await this.getReviewById(id)
+  }
+
+  async updateReviewByAdmin(id:string, review: UpdateReviewDto){
+      const existReview = await this.prisma.$queryRaw<Review[]>
+        `SELECT id FROM Review 
+        WHERE id = ${id}`
+      
+      if (!existReview || existReview.length === 0) {
+        throw new BadRequestException("Review doesn't exist")
+      }
+
+      await this.prisma.$executeRaw<Review>`
+        UPDATE Review SET 
+        rating = ${review.rating}, comment = ${review.comment}
+        WHERE id = ${id}
+      `
+      return await this.getReviewById(id)
+  }
+  
 }
