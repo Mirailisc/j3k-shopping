753175d09e914b84179cb97c7e36d99d7f38a1a8
Mirailisc <mirailisclm@gmail.com>
2025-04-15 22:56:07 +0700

    sdfgsdfgsd

--- frontend/src/components/Product/ReviewForm.tsx
+++ frontend/src/components/Product/ReviewForm.tsx
@@ -1,11 +1,8 @@
-'use client'
-
 import { useEffect, useState } from 'react'
 import { Star } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import { toast } from 'sonner'
