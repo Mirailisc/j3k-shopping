8a432869a6b9a32031939a069a200cc0f1162c55
Jirat Arayapitak <ham.jirat24@gmail.com>
2025-04-16 13:11:02 +0700

    merge conflict for review

--- backend/src/review/review.module.ts
+++ backend/src/review/review.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ReviewService } from './review.service'
+import { ReviewController } from './review.controller'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UserService } from 'src/user/user.service'
 import { SocialService } from 'src/social/social.service'
 import { ContactService } from 'src/contact/contact.service'
-import { ReviewController } from './review.controller'
 import { ProductService } from 'src/product/product.service'
 
 @Module({
